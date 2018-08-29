06f0fa9cdd1a91bfd222487840ef4da4d36e72ce
KBH <k01032633280@gmail.com>
2018-08-29 10:01:29 +0900

    header 'withour_cmake.h'

---
+++ header/without_cmake.h
@@ -0,0 +1,25 @@
+/* This header is for
+ * when you are not using CMAKE.
+ * */
+
+/* set 1 for your OS
+ * MAC is considered as UNIX
+ */
+#define OS_UNIX   0
+#define OS_WIN    0
+
+/* set 1 if you use BLAS
+ * */
+#define USE_CBLAS 0
+
+/* set 1 for your BLAS
+ * */
+#define USE_OPEN  0
+#define USE_MKL   0
+
+/* include this header at iip_type.h 
+ *
+ * NOTE)
+ * you need to link libraries manually.
+ * */
+
